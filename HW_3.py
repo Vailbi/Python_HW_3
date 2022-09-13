d65ef53fa6047be9a3ddb21ec866c831749055b2
Fedor Vailbi <vailbi94@gmail.com>
2022-09-13 20:05:47 +0300

    new homework 3

--- HW_3.py
+++ HW_3.py
@@ -1,9 +1,9 @@
 # №1 Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка,
 # стоящих на нечётной позиции.
 
-# arr = list(map(int, input().split()))
-# print(arr)
-# print(sum(arr[i] for i in range(1, len(arr), 2)))
+arr = list(map(int, input("Ведите числа через пробел: ").split()))
+print(arr)
+print(sum(arr[i] for i in range(1, len(arr), 2)))
 
 # №2 Напишите программу, которая найдёт произведение пар чисел списка.
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
@@ -40,20 +40,20 @@
 
 # №5 Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
-def NegFib(n):
-    if n == -2:
-        return -1
-    elif n == -1:
-        return 1
-    return NegFib(n + 2) - NegFib(n + 1)
-
-
-def fib(n):
-    if n == 1 or n == 2:
-        return 1
-    return fib(n - 1) + fib(n - 2)
-
-
-resultArr = [NegFib(i) for i in range(-8, 0)] + [0] + [fib(i) for i in range(1, 9)]
-
-print(resultArr)
+# def NegFib(n):
+#     if n == -2:
+#         return -1
+#     elif n == -1:
+#         return 1
+#     return NegFib(n + 2) - NegFib(n + 1)
+#
+#
+# def fib(n):
+#     if n == 1 or n == 2:
+#         return 1
+#     return fib(n - 1) + fib(n - 2)
+#
+#
+# resultArr = [NegFib(i) for i in range(-8, 0)] + [0] + [fib(i) for i in range(1, 9)]
+#
+# print(resultArr)
